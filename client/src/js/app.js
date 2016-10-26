ca7e34320b23474e0857710bfd0d6365099a66a0
gvickstrom <gvickstrom@gmail.com>
2016-10-26 14:23:46 -0600

    updated project structure

---
+++ client/src/js/app.js
@@ -0,0 +1,14 @@
+// sample angular code
+
+(function() {
+
+  'use strict';
+
+  angular
+    .module('DatingApp', [
+      'DatingApp.config',
+      'DatingApp.components.main',
+      
+    ]);
+
+})();
